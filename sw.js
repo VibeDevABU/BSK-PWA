7e7fa63d8cb343bd4aabf7d370ec2f24285f6c78
VibeDevABU <gainey91@gmail.com>
2025-12-13 22:25:10 +0200

    Add caching for service worker installation and fetch
    Implemented caching strategy for service worker.

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = 'bisc-sidekick-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('fetch', (e) => {
-  // Pass through requests
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
